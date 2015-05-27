090ec58b7d9d0f91ed552c5442d8c125575250f3
acernst <aernst1586@gmail.com>
2015-05-27 18:33:59 -0400

    FirstRevision_5_27

---
+++ MovieDatabase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testc();
+        }
+
+        public static void testc()
+        {
+            System.Console.WriteLine("This is a test of the console");
+        }
+    }
+}
